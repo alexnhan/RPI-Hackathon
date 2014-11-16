09848ff3d1ead4b2bc9c62c5bddd36c172c63cb5
Christopher J. Mariani <marianic0@elvis.rowan.edu>
2014-11-16 04:17:35 -0500

    Made large structural changes

--- index.php
+++ index.php
@@ -1,34 +1,40 @@
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+form{
+	display: inline;
+}
+</style>
+</head>
 <body>
 
-<form action="index.php" method="post">
-<input type="text" name="name"><br>
-<input type="submit">
+<form method="post">
+<input type="submit" name="button" value="clear"/>
+</form>
+
+<form method="post">
+<input type="text" name="text"/>
 </form>
 
 <?php
-$command = escapeshellcmd('python make.py ' . $_POST[name]);
-$output = shell_exec($command);
-$arry = explode("\n", $output);
+$arry = explode("\n", $_POST[text]);
 
 $fp = fopen('text', 'a') or die("Unable to open file!");
-if($arry[0] == "make"){
-	fwrite($fp,"\n");
-	for($i=1; $i<count($arry); $i++){
-		fwrite($fp, $arry[$i] . " ");
-	}
-} else if ($arry[0] == "print"){
-	$command = escapeshellcmd('python print.py');
-	$output = shell_exec($command);
-	$arry = explode("\n", $output);
-	for($i=1; $i<count($arry); $i++){
-                echo($arry[$i]);
-        }
 
-} else if ($arry[0] == "clear"){
-        ftruncate($fp, 0);
+if($_POST[button] == "clear"){
+	ftruncate($fp,0);
 }
+
+for($i=0; $i<count($arry); $i++)
+	if($arry[$i] != "")fwrite($fp, $arry[$i] . "\n");
+
+$command = escapeshellcmd('python print.py');
+$output = shell_exec($command);
+$arry = explode("\n", $output);
+$arry = array_reverse($arry);
+for($i=1; $i<count($arry); $i++)
+        echo($arry[$i] . "<br />");
 ?>
 
 </body>
--- make.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import sys
-
-commands = ', '.join(sys.argv[1:])
-print(commands.replace(', ',"\n"))
